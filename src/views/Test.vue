4ba9922bdbc4c412dd3e34f3e25f12041171f050
vivianink1991 <binggan26@126.com>
2020-09-14 22:41:08 +0800

    first commit

---
+++ src/views/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>This is a test page</h1>
+    <div>{{counter}}</div>
+    <div>DoubleCounter: {{doubleCounter}}</div>
+    <button @click="add">add</button>
+  </div>
+</template>
+<script>
+import { ref, computed, watch } from 'vue'
+
+export default {
+  name: 'Test',
+  setup () {
+    const counter = ref(0)
+    const add = () => {
+      counter.value++
+    }
+    const doubleCounter = computed(() => counter.value * 2)
+
+    watch(() => counter.value, val => {
+      console.log(`counter is ${val}`)
+    })
+    return {
+      counter,
+      add,
+      doubleCounter
+    }
+
+  }
+}
+</script>
